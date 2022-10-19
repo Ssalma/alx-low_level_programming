4cbcd8b1e849201dde55859d8aa763e7b7cbf474
Ssalma <salma@enyata.com>
2022-10-19 11:45:43 -0700

    absolute val of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * abs - _absolute value
+ * Return : int
+ * @n : haracter
+ * Description: func that return absolute value of a number
+ */
+
+int _abs(int)
+{
+	return ((n < 0) ? (n * (-1)) : (n));
+}
