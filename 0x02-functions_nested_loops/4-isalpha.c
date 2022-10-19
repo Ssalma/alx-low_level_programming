a751d758f0a3bd0e7b1721247d638ae0af9fcbbf
Ssalma <salma@enyata.com>
2022-10-19 10:21:57 -0700

    letter, lowercase, uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - function is alpha
+ * Description: func that checks letter, lowercase or uppercase
+ * @c: character to be checked
+ * Return: an int
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c< 122) || (c >= 65 && c<= 90))
+	{
+		return (1);
+	}
+	return (0);
+}
