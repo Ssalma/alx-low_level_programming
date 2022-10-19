d0e4c3c3cfc7fdbd4a43d9e73c882323859bdc55
Ssalma <salma@enyata.com>
2022-10-19 10:37:10 -0700

    5 check sign of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - func that prints sign of a number
+ * Return: 0 if 0, 1 if '+', -1 if '-'
+ * @n: is the number to be checked
+ * Description: func that checks the sign of number
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n = 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else (n < 1)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
