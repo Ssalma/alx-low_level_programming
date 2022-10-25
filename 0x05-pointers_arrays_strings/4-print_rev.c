6343d8b208deac720bf0d279cc9c57f8c5611212
Ssalma <salma@enyata.com>
2022-10-25 02:48:36 -0700

    edited 4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 #include <string.h>
 
 /**
@@ -11,6 +12,12 @@ void print_rev(char *s)
 {
 	int i, len, temp;
 	len = strlen(s);
+
+	if (len == 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
 	for (i = 0; i < (len/2); i++)
 	{
