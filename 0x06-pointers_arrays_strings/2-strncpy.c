49f5b74047fffbfea758da49443e158bdcca8a3c
Ssalma <salma@enyata.com>
2022-10-27 03:28:35 -0700

    fixed error

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,21 +3,28 @@
 
 /**
  * *_strncpy - copy function
- *@dest: first string
+ * @dest: first string
  * @src: second string
- *@n: bytes
+ * @n: bytes
  * Return: char
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	char i;
+	int j;
+	char *ptr = dest;
 
-	for (i = 0; i < n && dest[i] != '\0'; ++i){
-		src[i] = dest[i];
+	for (j = 0; j < n && src[j] != '\0'; j++, dest++)
+	{
+		*dest = src[j];
 	}
 
-	dest[i] = '\0';
+	while (j < n)
+	{
+		*dest = '\0';
+		dest++;
+		j++;
+	}
 
-	return (dest);
+	return (ptr);
 }
