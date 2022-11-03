df02d8c1b16a4428bdf879eb534c09e1803cfdc0
salma <salma@enyata.com>
2022-11-03 17:59:06 +0000

    fixed betty error

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
-* _puts_recursion - prints string
-* @s: char string
-* Return: void
-*/
+ * _puts_recursion - prints string using
+ * @s: char string
+ * Return: void
+ */
 
 void _puts_recursion(char *s)
 {
- if (*s == '\0')
- _putchar('\n');
- else
- {
-  _putchar(*s);
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
 		_puts_recursion(++s);
- }
+	}
 }
